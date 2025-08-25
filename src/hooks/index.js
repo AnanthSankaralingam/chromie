87c3e6851b0809d9d395c989d17573ee9f821516
AnanthS <ananth.sankaralingam@gmail.com>
2025-08-24 18:16:10 -0700

    mobile friendly home page; hook for detecing mobile device

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 // Export all hooks from this directory
 export { useChat } from './use-chat'
+export { useIsMobile } from './use-is-mobile'
 
 // Future hooks can be added here:
 // export { useAuth } from './use-auth'
